1a13b391c15f1e81b00ec4975bbbb38bf21adcab
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-04-23 16:03:21 +0200

    Improved tab completion

--- src/main/java/com/fadedbytes/theotherside/debug/Link/LinkCommand.java
+++ src/main/java/com/fadedbytes/theotherside/debug/Link/LinkCommand.java
@@ -1,6 +1,8 @@
 package com.fadedbytes.theotherside.debug.Link;
 
+import com.fadedbytes.theotherside.world.TheOthersideWorld;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,6 +10,8 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -70,11 +74,21 @@ public class LinkCommand implements CommandExecutor, TabCompleter {
                 }
 
                 if (args[0].equalsIgnoreCase("link")) {
-                    PlayerLink.link(playerToLink, slavePlayer, playerToLink.getWorld().getName());
-                    sender.sendMessage("§aPlayers linked successfully.");
+                    if (TheOthersideWorld.getWorld() == null) {
+                        sender.sendMessage("§cTheOthersideWorld is not loaded.");
+                    } else {
+                        if (playerToLink.equals(slavePlayer)) {
+                            sender.sendMessage("§cYou can't link yourself.");
+                            break;
+                        }
+                        PlayerLink.link(playerToLink, slavePlayer, TheOthersideWorld.getWorld().getName());
+                        sender.sendMessage("§aPlayers linked successfully.");
+                    }
                 } else if (args[0].equalsIgnoreCase("unlink")) {
                     PlayerLink.unlink(playerToLink);
                     sender.sendMessage("§aPlayers unlinked successfully.");
+                } else {
+                    sender.sendMessage("§c/link (link | unlink) <player> <slavePlayer> [world]");
                 }
             }
             case 4 -> {
@@ -94,6 +108,10 @@ public class LinkCommand implements CommandExecutor, TabCompleter {
 
                 if (args[0].equalsIgnoreCase("link")) {
                     try {
+                        if (playerToLink.equals(slavePlayer)) {
+                            sender.sendMessage("§cYou can't link yourself.");
+                            break;
+                        }
                         PlayerLink.link(playerToLink, slavePlayer, args[3]);
                         sender.sendMessage("§aPlayers linked successfully.");
                     } catch (IllegalArgumentException exception) {
@@ -109,8 +127,44 @@ public class LinkCommand implements CommandExecutor, TabCompleter {
         return true;
     }
 
+    private static List<String> OPTIONS = Arrays.asList("link", "unlink");
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        return null;
+        List<String> tabs = new ArrayList<>();
+
+        switch (args.length) {
+            case 1 -> {
+                for (String option : OPTIONS) {
+                    if (option.toLowerCase().startsWith(args[0].toLowerCase())) {
+                        tabs.add(option);
+                    }
+                }
+            }
+            case 2 -> {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (player.getName().toLowerCase().startsWith(args[1].toLowerCase())) {
+                        tabs.add(player.getName());
+                    }
+                }
+            }
+            case 3 -> {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (player.getName().toLowerCase().startsWith(args[2].toLowerCase())) {
+                        if (!args[1].equalsIgnoreCase(player.getName())) {
+                            tabs.add(player.getName());
+                        }
+                    }
+                }
+            }
+            case 4 -> {
+                if (!args[0].equalsIgnoreCase("link")) break;
+                for (World world : Bukkit.getWorlds()) {
+                    if (world.getName().toLowerCase().startsWith(args[3].toLowerCase())) {
+                        tabs.add(world.getName());
+                    }
+                }
+            }
+        }
+        return tabs;
     }
 }
